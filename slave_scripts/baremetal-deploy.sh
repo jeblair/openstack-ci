aaf2a27e3671a5008a1b2aadfe0ddac1d64b9fc9
James E. Blair <james.blair@rackspace.com>
2011-10-19 09:15:33 -0500

    Add syslog artifacts. Remove known_host weirdness, depend on .ssh/config instead.

--- slave_scripts/baremetal-deploy.sh
+++ slave_scripts/baremetal-deploy.sh
@@ -4,13 +4,20 @@ set -x
 #HEAD_HOST=${HEAD_HOST:-10.14.247.43}
 #COMPUTE_HOSTS=${COMPUTE_HOSTS:-10.14.247.44,10.14.247.45}
 
+WORKSPACE=`pwd`
+mkdir -p logs
+rm -f logs/*
 cd `dirname "$0"`
 
 echo "Jenkins: resetting hosts..."
 for host in $HEAD_HOST ${COMPUTE_HOSTS//,/ }; do
-    scp -o StrictHostKeyChecking=no lvm-kexec-reset.sh root@$host:/var/tmp/
-    ssh -o StrictHostKeyChecking=no root@$host /var/tmp/lvm-kexec-reset.sh
+    scp -o lvm-kexec-reset.sh root@$host:/var/tmp/
+    ssh -o root@$host /var/tmp/lvm-kexec-reset.sh
+    sudo rm -f /var/log/orchestra/rsyslog/$host/syslog
 done
+
+# Have rsyslog reopen log files we rm'd from under it
+sudo restart rsyslog
 
 # wait for the host to come up (2 ping responses or timeout after 5 minutes)
 echo "Jenkins: Waiting for head host to return after reset..."
@@ -21,7 +28,7 @@ fi
 
 echo "Jenkins: Pre-populating PIP cache"
 for host in $HEAD_HOST ${COMPUTE_HOSTS//,/ }; do
-    scp -o StrictHostKeyChecking=no -r ~/cache/pip root@$host:/var/cache/pip
+    scp -r ~/cache/pip root@$host:/var/cache/pip
 done
 
 echo "Jenkins: Executing build_bm_multi.sh."
@@ -29,4 +36,8 @@ echo "Jenkins: Executing build_bm_multi.sh."
 cd ~/devstack
 source ./functions.sh
 cache_images ~/devstack/files
-exec bash build_bm_multi.sh
+bash build_bm_multi.sh
+
+for host in $HEAD_HOST ${COMPUTE_HOSTS//,/ }; do
+    cp /var/log/orchestra/rsyslog/$host/syslog $WORKSPACE/logs/$host-syslog.txt
+done
